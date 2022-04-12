e1086d5cae0fede4d7e0128ef94539ac860f5ef4
Sayan Dey <dey.sayan22heaven@gmail.com>
2022-04-13 00:02:03 +0530

    container with most water

--- InterviewBit/Two Pointers/Container With Most Water.cpp
+++ InterviewBit/Two Pointers/Container With Most Water.cpp
@@ -1,18 +1,13 @@
 /*
 Given n non-negative integers a1, a2, ..., an,
-
 where each represents a point at coordinate (i, ai).
-
 'n' vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
 
 Find two lines, which together with x-axis forms a container, such that the container contains the most water.
-
-Your program should return an integer which corresponds to the maximum area of water that can be contained ( Yes, we know maximum area instead of maximum volume sounds weird. But this is 2D plane we are working with for simplicity ).
-
+Your program should return an integer which corresponds to the maximum area of water that can be contained.
 Note: You may not slant the container.
 
 Example :
-
 Input : [1, 5, 4, 3]
 Output : 6
 
